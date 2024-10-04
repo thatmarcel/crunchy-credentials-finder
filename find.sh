d8254503f6726851b1b9da4b8f34bfbffaf4244d
thatmarcel <contact@thatmarcel.com>
2024-10-04 19:40:49 +0200

    Make usable via Docker

--- find.sh
+++ find.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget "https://apkpure.com/crunchyroll/com.crunchyroll.crunchyroid/versions" --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.3" -O crunchyroll-versions.html
+wget "https://apkpure.com/crunchyroll/com.crunchyroll.crunchyroid/versions" --user-agent="Mozilla/5.0 (X11; Linux i686; rv:130.0) Gecko/20100101 Firefox/130.0" -O crunchyroll-versions.html
 rg "data-dt-versioncode=\"([0-9]{3,4})\"" crunchyroll-versions.html -o -I -N -r \$1 > version-codes.txt
 cut -d: -f 2 version-codes.txt | sort -n | tail -n 1 > latest-version-code.txt
-wget "https://d.apkpure.com/b/APK/com.crunchyroll.crunchyroid?versionCode=$(cat latest-version-code.txt)" --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.3" -O crunchyroll.apk
+wget "https://d.apkpure.com/b/APK/com.crunchyroll.crunchyroid?versionCode=$(cat latest-version-code.txt)" --user-agent="Mozilla/5.0 (X11; Linux i686; rv:130.0) Gecko/20100101 Firefox/130.0" -O crunchyroll.apk
 rm -f version-codes.txt latest-version-code.txt crunchyroll-versions.html
 apktool d -f crunchyroll.apk
 rm -rf output
@@ -15,7 +15,7 @@ rm -f matches.txt
 rg "versionCode: ([0-9]{3,4})" crunchyroll/apktool.yml -o -I -N -r \$1 > output/version-code.txt
 rg "versionName: ([0-9.]*)" crunchyroll/apktool.yml -o -I -N -r \$1 > output/version-name.txt
 rm -rf crunchyroll
-unzip -o crunchyroll.apk -d crunchyroll-app-contents
+unzip -o -q crunchyroll.apk -d crunchyroll-app-contents
 rm -f crunchyroll.apk
 printf "%x\n" $(rg $(cat output/client-id.txt) crunchyroll-app-contents/classes2.dex -a -o -I -N -b | rg "(.*):" -o -r \$1) > output/client-id-offset.txt
 printf "%x\n" $(rg $(cat output/client-secret.txt) crunchyroll-app-contents/classes2.dex -a -o -I -N -b | rg "(.*):" -o -r \$1) > output/client-secret-offset.txt
--- update.sh
+++ update.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+set -e
+
+if [[ ! -z $G_TOKEN ]]; then
+    echo $G_TOKEN | gh auth login --with-token
+    git config --global user.name $G_USER_NAME
+    git config --global user.email $G_USER_EMAIL
+fi
+
 ./find.sh
 
 git clone "https://github.com/thatmarcel/crunchy-credentials-offsets"
 cd crunchy-credentials-offsets
-rm -f client-id-offset.txt client-secret-offset.txt version-code.txt version-name.txt
-cp ../output/client-id-offset.txt .
-cp ../output/client-secret-offset.txt .
-cp ../output/version-code.txt .
-cp ../output/version-name.txt .
+cp -f ../output/client-id-offset.txt .
+cp -f ../output/client-secret-offset.txt .
+cp -f ../output/version-code.txt .
+cp -f ../output/version-name.txt .
 git add client-id-offset.txt client-secret-offset.txt version-code.txt version-name.txt
 git commit -m "Update offsets"
 git push origin main
